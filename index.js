bec395cb9271c302de9d6f99c9621f03dbd71752
Yago Lucas Gonçalves da Silva <yago543@gmail.com>
2024-02-15 08:55:53 -0300

    adicionado readme

---
+++ index.js
@@ -0,0 +1,17 @@
+function sum(a,b) {
+    return a + b
+}
+
+function multiplication(a, b) {
+    return a * b
+}
+
+function minus(a, b) {
+    return a - b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+module.export (sum, divide, minus, multiplication)
---
+++ readme.md
@@ -0,0 +1,2 @@
+# FIAP-NODE
+Este projeto tem o intuito de aprender como publicar pacotes no npm
